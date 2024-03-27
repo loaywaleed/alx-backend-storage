44da439855535c78f6e55fc6dabde8d9378aba04
loaywaleed <loaywaleed9@gmail.com>
2024-03-27 03:41:10 +0200

    updating by topic

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Script that updates all topics of a document based on name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Function that updates topics"""
+    mongo_collection.insertmany({"name": name}, {"$set": {"topics": topics}})
